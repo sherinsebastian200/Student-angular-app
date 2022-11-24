d5ef18b436b628df850ad21c9faaf77944cd4044
sherin200 <sherinsebastian21>
2022-11-24 15:01:56 +0530

    routing"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
 import { StudentViewComponent } from './student-view/student-view.component';
 
+const myRoute:Routes=[
+  {
+    path:"",
+    component:StudentEntryComponent
+
+  },
+  {
+    path:"view",
+    component:StudentViewComponent
+
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +27,8 @@ import { StudentViewComponent } from './student-view/student-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
