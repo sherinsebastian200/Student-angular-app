31ed017eeca3be523a00c7ff80a949a396ff9085
sherin200 <sherinsebastian21>
2022-11-24 14:53:50 +0530

    view all students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
+import { StudentViewComponent } from './student-view/student-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentEntryComponent
+    StudentEntryComponent,
+    StudentViewComponent
   ],
   imports: [
     BrowserModule,
